2e8c3ca0419ce2e6792e13e536d132bc45c02555
C01DZin <732345308@qq.com>
2019-07-08 14:51:31 +0800

    add play video

--- app/src/main/java/com/example/douyin/PlayVideo.java
+++ app/src/main/java/com/example/douyin/PlayVideo.java
@@ -4,7 +4,10 @@ import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.VideoView;
+
+import java.net.URI;
 
 public class PlayVideo extends AppCompatActivity {
     private VideoView videoView;
@@ -14,16 +17,28 @@ public class PlayVideo extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play_video);
         initBtn();
+        playVideo();
 
     }
     private void initBtn(){
-
+        videoView = findViewById(R.id.video_view);
+        videoView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(videoView.isPlaying()){
+                    videoView.pause();
+                }
+                else videoView.start();
+            }
+        });
     }
 
     private void playVideo(){
+
         String name =  getIntent().getStringExtra("user_name");
         String url =  getIntent().getStringExtra("video_url");
-        videoView.setVideoURI(url);
+        videoView.setVideoURI(Uri.parse(url));
+
         Log.d("jzh", "onActivityResult: " + url);
         videoView.start();
     }

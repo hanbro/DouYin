19309c64049c47a7f7c593313f93a275e2f86787
hanbro <295953895@qq.com>
2019-07-08 18:44:34 +0800

    VideoRecord

--- app/src/main/java/com/example/douyin/MainActivity.java
+++ app/src/main/java/com/example/douyin/MainActivity.java
@@ -155,7 +155,7 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.transcribe).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, PlayVideo.class));
+                startActivity(new Intent(MainActivity.this, CustomRecord.class));
             }
         });
     }
